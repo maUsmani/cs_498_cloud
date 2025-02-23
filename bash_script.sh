e82f9eb38e391ac9381317000d95ee052cceb17b
M Ammar Irfan <ausmani2@illinois.edu>
2025-02-23 17:44:27 -0600

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export HOME=/home/ec2-user
+GITHUB_TOKEN=""
 sudo yum update
 sudo yum install stress-ng -y
 sudo yum install htop -y
@@ -7,6 +8,8 @@ sudo yum install python3-pip -y
 pip3 install flask
 sudo yum install git -y
 cd /home/ec2-user
-git clone YOUR_CODECOMMIT_REPO_LINK (https://<github-personal-access-token>@github.com/username/repository_name)
-cd /home/ec2-user/YOUR_CODECOMMIT_REPO_NAME
+git clone https://$GITHUB_TOKEN@github.com/mausmani/cs_498_cloud cs_498_cloud
+
+cd /home/ec2-user/cs_498_cloud
+
 python3 serve.py
